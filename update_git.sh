71bc0f5744cafcf92f84d1670abae5831203eae8
dmoeller131 <dmoeller38@outlook.com>
2024-09-29 19:10:19 -0400

    testing

---
+++ update_git.sh
@@ -0,0 +1,15 @@
+# Use GNU Stow to symlink dotfiles
+stow .
+
+# Prompt for commit message
+echo "Commit Name:"
+read commit_name
+
+# Add changes to Git
+git add .
+
+# Commit with the provided message
+git commit -m "$commit_name"
+
+# Push to the master branch
+git push origin master
