cf7972d87d26699a60ef60721912223397dc4e1b
dmoeller131 <dmoeller38@outlook.com>
2024-09-30 16:16:27 -0400

    Nixos

--- update_git.sh
+++ update_git.sh
@@ -13,3 +13,14 @@ git commit -m "$commit_name"
 
 # Push to the master branch
 git push origin master
+
+read -p "Backup Nix cfg/s" yn
+
+case $yn in 
+	[yY] ) sudo cp -fr /etc/nixos/ ./;
+		break;;
+	[nN] ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+esac
+
