390d242242687a48b1755cfd3d12bebd4ba7106d
dmoeller131 <dmoeller38@outlook.com>
2024-09-30 16:23:59 -0400

    Nixos

--- update_git.sh
+++ update_git.sh
@@ -1,3 +1,26 @@
+# Prompt for backing up Nix configurations
+read -p "Backup Nix cfg/s (y/n)? " yn
+
+case $yn in 
+    [yY] ) 
+        echo "Backing up Nix configurations..."
+        sudo cp -fr /etc/nixos/ ./
+        echo "Backup completed."
+        ;;
+    [nN] ) 
+        echo "No backup taken."
+        ;;
+    * ) 
+        echo "Invalid response. Exiting."
+        exit 1
+        ;;
+esac
+
+read -p "Upload dot/s (y/n)? " yn2
+
+case $yn2 in 
+    [yY] ) 
+        echo "Uploading to git..."
 # Use GNU Stow to symlink dotfiles
 stow .
 
@@ -13,13 +36,13 @@ git commit -m "$commit_name"
 
 # Push to the master branch
 git push origin master
-
-read -p "Backup Nix cfg/s  y/n" yn
-
-case $yn in 
-	[yY] ) sudo cp -fr /etc/nixos/ ./;
-	[nN] ) echo exiting...;
-		exit;;
-	* ) echo invalid response;;
+        echo "Upload completed!"
+        ;;
+    [nN] ) 
+        echo "No upload done."
+        ;;
+    * ) 
+        echo "Invalid response. Exiting."
+        exit 1
+        ;;
 esac
-
