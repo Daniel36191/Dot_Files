fd4832b0fba4221e1b7e56f9f214f3f4a2766a6b
dmoeller131 <dmoeller38@outlook.com>
2024-10-08 18:57:16 -0400

    SImple instlal script

--- install.sh
+++ install.sh
@@ -11,13 +11,5 @@ sudo nixos-rebuild switch --flake /etc/nixos#default
 sudo chown -R daniel /etc/nixos
 echo "Cleaning"
 sudo nix-collect-garbage -d
-echo "Done"
-echo "Installing Spotify"
-flatpak install flathub com.spotify.Client
-flatpak update --commit=097c01a29cc9a847993ef65cd6ceefa1f451c16dfa58422c7d82230be5bd6e60 com.spotify.Client
-echo "Installing Proton-GE"
-protonup
-echo "Installing Waterfox"
-flatpak install flathub net.waterfox.waterfox
 echo "Applying Configs"
 stow .
