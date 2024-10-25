4f4d6e49a2e8c4a5b59dc62433d4d5635c19a4f8
dmoeller131 <dmoeller38@outlook.com>
2024-10-25 15:06:36 -0400

    Vm.nix overhall, restructured nix

---
+++ nixos/nixos/config/apps.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, inputs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+
+#User_Apps
+vesktop
+element-desktop
+obsidian
+librewolf
+pinta
+xournalpp
+obs-studio
+vlc
+video-trimmer
+
+#Sound
+qpwgraph
+easyeffects
+rnnoise
+spicetify-cli
+
+#Games
+protonup #Steam_Apps
+mangohud #fps_Overlay
+
+#Devolpment
+vscode
+
+
+############
+# Non User #
+############
+
+#Themeing
+catppuccin-cursors.macchiatoDark
+catppuccin-gtk
+nwg-look
+neofetch
+btop
+mission-center
+nerdfonts
+(nerdfonts.override { fonts = [ "CascadiaCode" ]; })
+
+#Hyprland
+hyprcursor
+hyprpaper
+hyprpicker
+waybar
+wev
+dunst #Notifcations
+libnotify #for dunst
+
+#Clipboard/screenchots
+wl-clipboard
+cliphist
+grimblast
+grim
+slurp
+];
+
+# For Virtual Here To work
+boot.extraModulePackages = with config.boot.kernelPackages; [ usbip ];
+programs.nix-ld.enable = true;
+
+#Fonts
+fonts.packages = with pkgs; [
+  nerdfonts
+];
+# Flatpacks
+services.flatpak.enable = true;
+
+#Cli prompt
+programs.starship.enable = true;
+
+#Thunar file manager
+programs.thunar = {
+  enable = true;
+  plugins = with pkgs.xfce; [ thunar-archive-plugin thunar-volman ];
+};
+programs.xfconf.enable = true;
+services.gvfs.enable = true;
+
+#Steam
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+}
